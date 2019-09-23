68a5f2caecfa7e18b772f5555b467c741a092aa4
Rafał Galczak <r.galczak@gmail.com>
2019-09-23 18:22:29 +0200

    Fixed make_greeting function for the unit tests to pass

--- 02-libraries_and_tests/src/hello/hello.cc
+++ 02-libraries_and_tests/src/hello/hello.cc
@@ -9,5 +9,8 @@ void hello::greet(std::string const& name) {
 
 std::string hello::make_greeting(const std::string &name) {
     using namespace std::string_literals;
+    if (name.empty()) {
+        return hello::EMPTY_NAME_MSG;
+    }
     return "Hello "s + name;
 }
